8807d93ae1a8947cb26ccb48e5397cac90f916e3
vanillakernel <vanillakernel@gmail.com>
2016-01-06 10:19:03 -0800

    tons of works to do

--- connect4.py
+++ connect4.py
@@ -18,16 +18,49 @@ def random_move (current_board, player):
 	print "New board: \n %r" % print_board(current_board)
     else:
 	random_move(current_board, player) # try again
+
+
+# This will play the game forward to D depth from the current board.
+def game_tree(current_board, player, depth):
+   return "" 
+   
+# This defines the value of a board given a few rules.
+def define_value(board, player):
+    value = 0
+    player_count = 0
+    enemy_count = 0
+
+# TODO see if I can do the static evaluation once based on player.
+
+    # If the board is a win, score it at 100, and abitrary number.
+	# Check for four  horizontal.
+    for row in board.iterrows():
+	print row  
+	# Check for four vertical
+	# Check for four diagonal
+
+    # If the board is a loss, score it at -100
+
+    # If we place a piece next to another piece, it is a slightly higher value.
     
-    
+
+    # For minmax, we will need to make the score high for player 0, low for 1
+    if player:
+	return value*(-1)
+    return value
+
+
+# Check which cell (if any), we can drop to in a column.
 def check_cell(column,index):
-    # If we are at the last row, just return that.
+    # If we are at the last row and it is still empty, just return that
     if (index == len(column)-1):
 	return index
-    if (not math.isnan(column[0])): #if the 0th is full, column is full. 
+    if (not math.isnan(column[0])): #if top cell full, column is full. 
 	return False
+    # If current cell is empty, but the next one down is full, return index.
     if (math.isnan(column[index]) and not math.isnan(column[index+1])):
 	return index 
+    #Otherwise, check the next cell down.
     return check_cell(column, index+1)
    
     
@@ -51,11 +84,13 @@ def main ():
     if (not player):
      print "Computer goes first."
 
+    define_value(board, player)
+
     i=0
-    while (i<30): 
-	random_move(board, player)
-	i+=1
-	player = int(not player)
+    #while (i<30): 
+#	random_move(board, player)
+#	i+=1
+#	player = int(not player)
 
 if __name__ == "__main__":
         main()
