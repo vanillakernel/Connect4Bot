200ade09e74f65631602853a9b9be6e18557b0b4
vanillakernel <vanillakernel@gmail.com>
2016-01-28 07:53:46 -0800

    Just starting to add depth.

--- connect4_OO.py
+++ connect4_OO.py
@@ -6,18 +6,6 @@ from itertools import groupby
 import copy
 
 
-# Check which cell (if any), we can drop to in a column.
-def check_cell(column, index=0):
-    # If we are at the last row and it is still empty, just return that
-    if (index == len(column)-1):
-	return index
-    if (column[0] is not None): #if top cell full, column is full. 
-	return False
-    # If current cell is empty, but the next one down is full, return index.
-    if (column[index] is None and column[index+1] is not None):
-	return index 
-    #Otherwise, check the next cell down.
-    return check_cell(column, index+1)
 
 
 ###############################################################################
@@ -37,10 +25,11 @@ class Player (object):
   # Make a random move.
   def random_move (self, board):
       moves = board.valid_moves()
-      self.play_single_move(board,random.choice(moves))
-      print "The random move is:"
+      move = random.choice(moves)
+      self.play_single_move(board,move)
+      print "The random move is: %r,%r" % move
       board.print_board()
-      score = self.board_score(board)
+      score = self.board_score(board, self.marker)
       #print "This move has a score of %r" % score
       #print ("Playing the RANDOM MOVE, as the {0} with {1} as my opponent, I score this board at {2} "
 #	     ).format("Human" if self.marker else "Computer",
@@ -54,7 +43,6 @@ class Player (object):
 
 
   # MinMax
-
   def minmax(self, board):
     moves = board.valid_moves()
     best_board = copy.deepcopy(board)
@@ -66,7 +54,7 @@ class Player (object):
     for move in moves:
 	temp_board = copy.deepcopy(board)
 	temp_board = self.play_single_move(temp_board, move)
-	temp_board.score = self.board_score(temp_board)
+	temp_board.score = self.board_score(temp_board,self.marker)
 	if (temp_board.score == True):
 	    best_score = temp_board.score
 	    best_board = temp_board
@@ -90,9 +78,60 @@ class Player (object):
     best_board.print_board()
     return move 
   
+  # MinMaxDeep
+  def minmax_deep(self, board, depth, score):
+    last_score = score
+    # Turn one is always us.
+    if depth%2==0:
+	turn = not self.marker
+    else:
+	turn = self.marker
+    print "depth %r , turn %r" % (depth,turn) 
+    print "{0}'s turn.".format("Computer" if turn == 0 else "Human")
+    moves = board.valid_moves()
+    best_board = copy.deepcopy(board)
+    best_move = random.choice(moves) #yolo
+    
+    if (turn): # Human wants the max.
+	best_score=(-1)
+    else: # Computer wants the min.
+	best_score=(1)
+	
+    if (depth > 0):
+	for move in moves:
+	    temp_board = copy.deepcopy(board)
+	    temp_board = self.play_single_move(temp_board, move)
+	    temp_board.score = self.board_score(temp_board, turn)
+
+	    if (temp_board.score == True):
+		best_score = temp_board.score
+	        best_board = temp_board
+		best_move = move
+		print "The WINNING  move is: %r, %r:" % move
+		best_board.print_board()
+		return move
+	    if (turn and temp_board.score > best_score):#Maximize
+		print "Found a better score: {0} is better than {1} for the {2}".format(
+		temp_board.score, best_score, "Computer" if turn == 0 else "Human")
+		best_score = temp_board.score
+		best_board = temp_board
+		best_move = move
+	    if (not turn and temp_board.score < best_score): #minimize
+		print "Found a better score: {0} is better than {1} for the {2}".format(
+		temp_board.score, best_score, "Computer" if turn == 0 else "Human")
+		best_score = temp_board.score
+		best_board = temp_board
+		best_move = move
+	self.minmax_deep(temp_board, depth-1, best_score)
+
+    print "The ideal move is: %r, %r:" % best_move
+    best_board.print_board()
+    return best_move 
+  
+  
   # Time to score.
-  def board_score(self, board):
-     if self.marker == 0:
+  def board_score(self, board, turn):
+     if turn == 0: # This is the computer
 	 sign = -1
 	 enemy = 1
      else:
@@ -104,7 +143,7 @@ class Player (object):
 	matches = [(key,len(list(group))) for key, group in groupby(row)]
 	for match in matches:
 	    key,count=match
-	    if count>=4 and key is self.marker: # Board is a win for us.
+	    if count>=4 and key is turn: # Board is a win for us.
 		print "HNGGGGGG"
 		#score += (20000*sign)
 		return True
@@ -112,7 +151,7 @@ class Player (object):
 		#print "Our enemy, the {0}, has a triple!".format(
 		#	"Human" if enemy else "Computer")
 		score += (200* (-sign))
-	    if count==3 and key is self.marker: # We may win!
+	    if count==3 and key is turn: # We may win!
 		#print "We, the {0}, are so close!".format(
 		#	"Human" if self.marker else "Computer")
 		score += (200*sign)
@@ -120,7 +159,7 @@ class Player (object):
 		#print "Our enemy, the {0}, has a pair".format(
 		#	"Human" if enemy else "Computer")
 		score += (1* (-sign))
-	    if count==2 and key is self.marker: # We have a pair
+	    if count==2 and key is turn: # We have a pair
 		#print "We, the {0}, have a pair".format(
 		#	"Human" if self.marker else "Computer")
 		score += (1*sign)
@@ -196,6 +235,18 @@ class Game_Board (object):
         column.append(row[index])
       return column
 
+    # Check which cell (if any), we can drop to in a column.
+  def check_cell(self, column, index=0):
+    # If we are at the last row and it is still empty, just return that
+      if (index == len(column)-1):
+  	  return index
+      if (column[0] is not None): #if top cell full, column is full. 
+	  return False
+      # If current cell is empty, but the next one down is full, return index.
+      if (column[index] is None and column[index+1] is not None):
+ 	  return index 
+      #Otherwise, check the next cell down.
+      return self.check_cell(column, index+1)
 
   # This will play the game forward to D depth from the current board.
   def game_tree(self, player, depth):
@@ -207,7 +258,7 @@ class Game_Board (object):
     # For each column, try to drop a token, and return r/c if valid.
     for c in range (0,self.columns):
       column = self.get_column(c);
-      r = check_cell(column)
+      r = self.check_cell(column)
       if (r): #if the move is valid add it to the list of valid moves
         valid_moves.append((r,c))
     return valid_moves
@@ -253,14 +304,16 @@ def main ():
     # Make the computer play against itself.
     i=0
     win = False
-    while (i<10 and win==False): 
+    while (i<2 and win==False): 
       if (current_player):
 	print "Human's turn."
-	human.minmax(board)  
+	#human.minmax(board)
+	human.minmax_deep(board, 2, 0)
 	score=human.random_move(board)
       else:
 	print "Computer's turn."
-	computer.minmax(board)
+	#computer.minmax(board)
+	human.minmax_deep(board, 2, 0)
         score=computer.random_move(board)
       i+=1
       current_player = int(not current_player)
